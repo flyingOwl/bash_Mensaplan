27dab6ca6978c082426740aa66a7ae3af66c8323
flyingOwl <no@email>
2013-03-28 17:55:51 +0100

    Improved performance even more, parsing and rendering now takes less than 200ms Using direct parsing in HTML-code

--- mensaplan.bash
+++ mensaplan.bash
@@ -45,9 +45,18 @@ fi
 if [ ! -z "$tePreise" ];
 then
 	prPreise=""
-	echo "$tePreise" | grep "S" > /dev/null && prPreise+=S
-	echo "$tePreise" | grep "M" > /dev/null && prPreise+=M
-	echo "$tePreise" | grep "F" > /dev/null && prPreise+=F
+	if [[ "$tePreise" == *S* ]]
+	then
+		prPreise+=S
+	fi
+	if [[ "$tePreise" == *M* ]]
+	then
+		prPreise+=M
+	fi
+	if [[ "$tePreise" == *F* ]]
+	then
+		prPreise+=F
+	fi
 fi
 
 
@@ -57,9 +66,10 @@ let numberSection--
 let BIG_TAB=2+6*$(echo "$prPreise" | wc -c)-6
 
 printSection(){
-for (( c=1; c<=$anzahl; c++ ))
+echo "$del1: ($(($anzahl/2)))"
+for (( c=1; c<=$anzahl; c=c+2 ))
 	do
-		nowPreis=$(echo "$1" | awk "NR==$c")
+		nowPreis=$(echo "$1" | awk "NR==$((c+1))")
 		studPreis=${nowPreis:4:4}
 		mitarPreis=${nowPreis:11:4}
 		if [ -z $mitarPreis ];
@@ -73,33 +83,47 @@ for (( c=1; c<=$anzahl; c++ ))
 		fi
 		
 		nPreis=''
-		echo "$prPreise" | grep "S" > /dev/null && nPreis+="$studPreis  "
-		echo "$prPreise" | grep "M" > /dev/null && nPreis+="$mitarPreis  "
-		echo "$prPreise" | grep "F" > /dev/null && nPreis+="$fremdPreis  "
+		if [[ "$prPreise" == *S* ]]
+		then
+			nPreis+="$studPreis  "
+		fi
+		if [[ "$prPreise" == *M* ]]
+		then
+			nPreis+="$mitarPreis  "
+		fi
+		if [[ "$prPreise" == *F* ]]
+		then
+			nPreis+="$fremdPreis  "
+		fi
 		echo -ne "€ $nPreis\033[$TERM_COL""D\033[$BIG_TAB""C"
-		echo "$2"  | awk "NR==$c"
+		echo "$1"  | awk "NR==$c"
 	done
 }
 
-wget -T 10 -nv --no-cache --output-document="$tempFile" "$webSite" > /dev/null 2>&1 || exit 1
 
-content=$(grep -m 1 -A 400 -B 20 "Tagesübersicht" "$tempFile" | sed -e 's/<[^>]\+>//g' -e 's/^ *//' | uniq)
+#wget -T 10 -nv --no-cache --output-document="$tempFile" "$webSite" > /dev/null 2>&1 || exit 1
 
-datum=$(echo "$content" | grep -m 1 "Tagesübersicht")
+content=$(grep -m 1 -A 400 "Tagesübersicht" "$tempFile") #| sed -e 's/<[^>]\+>//g' -e 's/^ *//' -e '/^$/d')
+
 echo
-echo $datum
+echo "$content" | grep -m 1 -o -E "Tagesübersicht [a-Z]*,[ .0-9]*"
 
 for (( sec=1; sec<=$numberSection; sec++ ))
 do
 	del1=$(echo -e "$section" | awk "NR==$sec" )
-	echo "$print" | grep "$del1" > /dev/null || continue
+	if [[ "$print" != *"$del1"* ]]
+	then
+		continue
+	fi
 	echo
 	del2=$(echo -e "$section" | awk "NR==$((sec+1))")
-	cC=$(echo "$content" | sed -e '1,/'$del1'/d'  -e '/'$del2'/,$d')
-	texte=$(echo "$cC" | awk '0 == (NR+1) % 3' | sed -e 's/ *[0-9]\+  \+//g')
-	preise=$(echo "$cC" | awk '0 == NR % 3' | tr -d '\r')
+	cC=$(echo "$content" | sed -e '1,/.*'$del1'.*/d'  -e '/.*'$del2'.*/,$d')
+	texte=$(echo "$cC" | sed -e 's/.*<\/a> *\([^<]\+\)<a href="#zus.*/\1/' -e 's/.*preis">\([^<]\+\)<\/td>.*/\1/' -e '/^ \+/d')
+	#echo "$texte"
+	
+	#preise=$(echo "$cC" | sed -e 's/.*preis">\([^<]\+\)<\/td>.*/\1/' -e '/^ \+/d')
 	anzahl=$(echo "$texte" | wc -l)
-	echo "$del1: ($anzahl)"
-	printSection "$preise" "$texte"
+	printSection "$texte"
+	#exit
 done
 echo
