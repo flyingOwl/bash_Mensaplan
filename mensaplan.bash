abe1668ab433d431149f7f53d2f7a6907f00c3a3
flyingOwl <no@email>
2013-03-29 14:42:30 +0100

    Changed internal design, removed checks that will never differ in their result

--- mensaplan.bash
+++ mensaplan.bash
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+# Change this two lines for any other mensa powered by StudentenWerk 
 webSite="http://www.studentenwerk-berlin.de/mensen/speiseplan/beuth/index.html"
 webNext="http://www.studentenwerk-berlin.de/mensen/speiseplan/beuth/01.html"
+
 tempFile="/tmp/mensaplan.temp"
 section="Salate\nAktionsstand\nEssen\nBeilagen\nDesserts\nKennzeichnungen"
 print=""
 prPreise="SMF"
 tePreise=""
-TERM_COL=$(tput cols)
 
 if [ $# -ne 0 ] ; then
 while getopts AEBDSnp:h opt
@@ -44,30 +45,16 @@ fi
 
 if [ ! -z "$tePreise" ];
 then
-	prPreise=""
-	if [[ "$tePreise" == *S* ]]
-	then
-		prPreise+=S
-	fi
-	if [[ "$tePreise" == *M* ]]
-	then
-		prPreise+=M
-	fi
-	if [[ "$tePreise" == *F* ]]
-	then
-		prPreise+=F
-	fi
+	prPreise=$tePreise
 fi
 
 
 numberSection=$(echo -e "$section" | wc -l)
 let numberSection--
 
-let BIG_TAB=2+6*$(echo "$prPreise" | wc -c)-6
-
 printSection(){
 echo "$del1: ($(($anzahl/2)))"
-for (( c=1; c<=$anzahl; c=c+2 ))
+for (( c=1; c<$anzahl; c=c+2 ))
 	do
 		nowPreis=$(echo "$1" | awk "NR==$((c+1))")
 		studPreis=${nowPreis:4:4}
@@ -95,18 +82,16 @@ for (( c=1; c<=$anzahl; c=c+2 ))
 		then
 			nPreis+="$fremdPreis  "
 		fi
-		echo -ne "€ $nPreis\033[$TERM_COL""D\033[$BIG_TAB""C"
+		echo -ne "€ $nPreis"
 		echo "$1"  | awk "NR==$c"
 	done
 }
 
-
 wget -T 10 -nv --no-cache --output-document="$tempFile" "$webSite" > /dev/null 2>&1 || exit 1
-
-content=$(grep -m 1 -A 400 "Tagesübersicht" "$tempFile") #| sed -e 's/<[^>]\+>//g' -e 's/^ *//' -e '/^$/d')
+content=$(grep -m 1 -A 400 "Tagesübersicht" "$tempFile")
 
 echo
-echo "$content" | grep -m 1 -o -E "Tagesübersicht [a-Z]*,[ .0-9]*"
+grep -m 1 -o -E "Tagesübersicht [a-Z]*,[ .0-9]*" <<< "$content"
 
 for (( sec=1; sec<=$numberSection; sec++ ))
 do
@@ -117,13 +102,10 @@ do
 	fi
 	echo
 	del2=$(echo -e "$section" | awk "NR==$((sec+1))")
-	cC=$(echo "$content" | sed -e '1,/.*'$del1'.*/d'  -e '/.*'$del2'.*/,$d')
-	texte=$(echo "$cC" | sed -e 's/.*<\/a> *\([^<]\+\)<a href="#zus.*/\1/' -e 's/.*preis">\([^<]\+\)<\/td>.*/\1/' -e '/^ \+/d')
-	#echo "$texte"
-	
-	#preise=$(echo "$cC" | sed -e 's/.*preis">\([^<]\+\)<\/td>.*/\1/' -e '/^ \+/d')
-	anzahl=$(echo "$texte" | wc -l)
+	cC=$(sed -e '1,/.*'$del1'.*/d'  -e '/.*'$del2'.*/,$d' <<< "$content")
+	texte=$(sed -e 's/.*<\/a> *\([^<]\+\)<a href="#zus.*/\1/' -e 's/.*preis">\([^<]\+\)<\/td>.*/\1/' -e '/^ \+/d' <<< "$cC")
+	anzahl=$(wc -l <<< "$texte")
 	printSection "$texte"
-	#exit
 done
 echo
+
